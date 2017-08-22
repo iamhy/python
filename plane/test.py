5162e72e77d7634852afd36544df4e352b236ec6
houyi <houyi@msn.com>
2017-08-23 00:14:51 +0800

    对大图进行了切片

--- plane/test.py
+++ plane/test.py
@@ -1,30 +1,32 @@
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
-im = Image.open("a330-300.png").convert('L')
-im2 = Image.open("WC.png").convert('L')
+planes = Image.open("a330-300.png")
+im1 = planes.convert('L')
+wc = Image.open("WC.png")
+im2 = wc.convert('L')
+width, height = im1.size
+width1, height1 = im2.size
 
-width, height = im.size
-width1, height = im2.size
+img = []
 
 n = 0
 
-for x in range(0, width):
-    for y in range(0, height):
-        for i in range(0, width1):
-            for j in range(0, height):
-                if im.getpixel((x, y)) == im2.getpixel((i, j)):
-                    continue
-                    n+=1
-                else:
-                    break
-    
-            break
-    
-# p = np.array(im)
-# q = np.array(im2)
+plt.figure("图片列表")
+
+for x in range(0, width, width1):
+    for y in range(0, height, height1):
+        box = (width, height, width + width1, height + height1)
+        roi = planes.crop(box)
+        n += 1
+
+        plt.subplot(204,10,n),plt.title(str(n))
+        plt.imshow(roi)
+plt.show()
 print(n)
 
-# im.show()
 
-# print(q)
+# p = np.array(im1)
+# # q = np.array(im2)
+# print(im1.size,im2.size)
